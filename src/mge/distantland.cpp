Hrnchamd/MGE-XE

commit 6f5453f18b64d3c9afcc51538f602feb9b04a44d

Corrected atmosphere scattering fog during menu mode
In atmosphere scattering mode, the fog colour adjustment was being run on the scenegraph fog colour. This was not updated during menu mode resulting in stacking adjustments every frame. It was entirely unnoticeable unless there was a weather transition where one weather type had niceWeather == 1.

Solved by reading the uncorrected colour, and removing the unreliable getSceneFogCol. Fog updates are now skipped during menu mode, but that is not a requirement for correctness.

src/mge/distantland.cpp
@@ -424,6 +424,11 @@ void DistantLand::adjustFog()
 
     nearViewRange = mwBridge->GetViewDistance();
 
+    // Morrowind does not update weather during menu mode and the frame after
+    // No need to run adjustment during menu mode
+    if(mwBridge->IsMenu())
+        return;
+
     // Get fog cell ranges based on environment and weather
     if(mwBridge->IsUnderwater(eyePos.z)) {
         fogStart = Configuration.DL.BelowWaterFogStart;
@@ -519,7 +524,8 @@ void DistantLand::adjustFog()
     }
 
     // Adjust Morrowind fog colour towards scatter colour if necessary
-    RGBVECTOR c = mwBridge->getSceneFogCol();
+    // This is a read of the unadjusted colour
+    RGBVECTOR c = *mwBridge->CurFogColVector();
 
     if((Configuration.MGEFlags & USE_DISTANT_LAND) && (Configuration.MGEFlags & USE_ATM_SCATTER) && mwBridge->CellHasWeather() && !mwBridge->IsUnderwater(eyePos.z))
     {
@@ -528,7 +534,7 @@ void DistantLand::adjustFog()
         // Simplified version of scattering from the shader
         const RGBVECTOR *skyCol = mwBridge->CurSkyColVector();
         const D3DXVECTOR3 newSkyCol = 0.38 * D3DXVECTOR3(skyCol->r, skyCol->g, skyCol->b) + D3DXVECTOR3(0.23, 0.39, 0.68);
-        const float sunaltitude = pow(1 + sunPos.z, 10);
+        const float sunaltitude = powf(1 + sunPos.z, 10);
         const float sunaltitude_a = 2.8 + 4.3 / sunaltitude;
         const float sunaltitude_b = saturate(1 - exp2(-1.9 * sunaltitude));
         const float sunaltitude2 = saturate(exp(-2 * sunPos.z)) * saturate(sunaltitude);
@@ -575,7 +581,7 @@ void DistantLand::adjustFog()
         // Alter Morrowind's fog colour through its scenegraph
         // This way it automatically restores the correct colour if it has to switch fog modes mid-frame
         DWORD fc = (DWORD)nearfogCol;
-        mwBridge->setSceneFogCol(fc);
+        mwBridge->setScenegraphFogCol(fc);
 
         // Set device fog colour to propagate change immediately
         device->SetRenderState(D3DRS_FOGCOLOR, fc);

src/mge/mwbridge.cpp
@@ -104,13 +104,14 @@ void MWBridge::Load()
     eShadowSlider = eTruRenderWidth + 0xC;        //0x84
     eCrosshair1 = eShadowSlider + 0x10;         //0x94
     eAI = eCrosshair1 + 0x4;            //0x98
+    eMenu = eMaster1 + 0xd6;
     eView0 = eAI + dwHRotScaleOff + 0x5C;  //0xF4
     eRenderWidth = eView0 + 0x4;                 //0xF8
     eView1 = eRenderWidth + 0x188;         //0x280
     eCombat = eView1 + 0x80;                //0x300
 
     eGamma = eMaster2 + 0x3C;  //0x3C  //00D1E090
-    eView4 = eGamma+ + 0x10;   //0x4C  //00D1E0A0
+    eView4 = eGamma + 0x10;   //0x4C  //00D1E0A0
     eLookMenu = eView4 + 0x9C;    //0xE8  //00D1E090
 
     eX = read_dword(eView0 - 0x10) + 0x1C;
@@ -123,7 +124,6 @@ void MWBridge::Load()
 
     eView3 = read_dword(eView1 - 0x10) + 0x114;
     eExt = read_dword(eView1 + 0x10) + 0x10;
-    eMenu = read_dword(read_dword(eView1 + 0x14) + 0x10) + 0x58;
     eMouseLim = read_dword(eD3D + 0x20) + 0x24;
 
     eGammaFunc = read_dword(read_dword(eMaster2) + 0x50);
@@ -322,7 +322,7 @@ bool MWBridge::IsExterior()
 bool MWBridge::IsMenu()
 {
     assert(m_loaded);
-    return (read_dword(eMenu) & 0x10000) != 0;
+    return read_byte(eMenu);
 }
 
 //-----------------------------------------------------------------------------
@@ -443,16 +443,7 @@ RGBVECTOR* MWBridge::CurFogColVector()
 
 //-----------------------------------------------------------------------------
 
-DWORD MWBridge::getSceneFogCol()
-{
-    DWORD addr = read_dword(eEnviro) + 0x9c;
-    addr = read_dword(addr) + 0x1c;
-    return read_dword(addr);
-}
-
-//-----------------------------------------------------------------------------
-
-void MWBridge::setSceneFogCol(DWORD c)
+void MWBridge::setScenegraphFogCol(DWORD c)
 {
     DWORD addr = read_dword(eEnviro) + 0x9c;
     addr = read_dword(addr) + 0x1c;

src/mge/mwbridge.h
@@ -46,8 +46,7 @@ public:
     float GetWeatherRatio();
     RGBVECTOR* CurSkyColVector();
     RGBVECTOR* CurFogColVector();
-    DWORD getSceneFogCol();
-    void setSceneFogCol(DWORD c);
+    void setScenegraphFogCol(DWORD c);
     bool CellHasWeather();
     float* GetWindVector();
     DWORD GetWthrStruct(int wthr);

commit 351e790a796d65cc54cc4b487caa66a5cc9d0411

Per-pixel lighting fog colour fix
Fixes a regression with interior fog colour introduced in 6f5453f18b64d3c9afcc51538f602feb9b04a44d. The fog colour read from MWBridge was the current weather fog colour, but was not updated for interiors, causing incorrect fog in interiors.

Solved by falling back to reading the scenegraph again, and more descriptive naming of the MWBridge fog functions.

src/mge/distantland.cpp
@@ -367,12 +367,12 @@ void DistantLand::setupCommonEffect(const D3DXMATRIX *view, const D3DXMATRIX *pr
     // Sky/fog
     float fogS = (Configuration.MGEFlags & EXP_FOG) ? (fogStart / Configuration.DL.ExpFogDistMult) : fogStart;
     float fogE = (Configuration.MGEFlags & EXP_FOG) ? (fogEnd / Configuration.DL.ExpFogDistMult) : fogEnd;
-    float *sky = mwBridge->CellHasWeather() ?  (float*)mwBridge->CurSkyColVector() : (float*)&horizonCol;
+    const RGBVECTOR *skyCol = mwBridge->CellHasWeather() ?  mwBridge->getCurrentWeatherSkyCol() : &horizonCol;
     effect->SetFloat(ehFogStart, fogS);
     effect->SetFloat(ehFogRange, fogE);
     effect->SetFloat(ehFogNearStart, fogNearStart);
     effect->SetFloat(ehFogNearRange, fogNearEnd);
-    effect->SetFloatArray(ehSkyCol, sky, 3);
+    effect->SetFloatArray(ehSkyCol, *skyCol, 3);
     effect->SetFloatArray(ehFogCol1, nearfogCol, 3);
     effect->SetFloatArray(ehFogCol2, horizonCol, 3);
     effect->SetFloat(ehNearViewRange, nearViewRange);
@@ -524,15 +524,14 @@ void DistantLand::adjustFog()
     }
 
     // Adjust Morrowind fog colour towards scatter colour if necessary
-    // This is a read of the unadjusted colour
-    RGBVECTOR c = *mwBridge->CurFogColVector();
-
     if((Configuration.MGEFlags & USE_DISTANT_LAND) && (Configuration.MGEFlags & USE_ATM_SCATTER) && mwBridge->CellHasWeather() && !mwBridge->IsUnderwater(eyePos.z))
     {
-        RGBVECTOR c0(c), c1(c);
+        // Read unadjusted colour, as the scenegraph fog colour may not be updated during menu transitions
+        RGBVECTOR c0 = *mwBridge->getCurrentWeatherFogCol();
+        RGBVECTOR c1 = c0;
 
         // Simplified version of scattering from the shader
-        const RGBVECTOR *skyCol = mwBridge->CurSkyColVector();
+        const RGBVECTOR *skyCol = mwBridge->getCurrentWeatherSkyCol();
         const D3DXVECTOR3 newSkyCol = 0.38 * D3DXVECTOR3(skyCol->r, skyCol->g, skyCol->b) + D3DXVECTOR3(0.23, 0.39, 0.68);
         const float sunaltitude = powf(1 + sunPos.z, 10);
         const float sunaltitude_a = 2.8 + 4.3 / sunaltitude;
@@ -588,8 +587,8 @@ void DistantLand::adjustFog()
     }
     else
     {
-        // Save colour for matching near fog in shaders
-        nearfogCol = c;
+        // Save current fog colour for matching near fog in shaders
+        nearfogCol = RGBVECTOR(mwBridge->getScenegraphFogCol());
     }
 }
 

src/mge/mwbridge.cpp
@@ -427,18 +427,27 @@ float MWBridge::GetWeatherRatio()
 
 //-----------------------------------------------------------------------------
 
-RGBVECTOR* MWBridge::CurSkyColVector()
+const RGBVECTOR * MWBridge::getCurrentWeatherSkyCol()
 {
     assert(m_loaded);
-    return (RGBVECTOR*)eCurSkyCol;
+    return (RGBVECTOR *)eCurSkyCol;
 }
 
 //-----------------------------------------------------------------------------
 
-RGBVECTOR* MWBridge::CurFogColVector()
+const RGBVECTOR * MWBridge::getCurrentWeatherFogCol()
 {
     assert(m_loaded);
-    return (RGBVECTOR*)eCurFogCol;
+    return (RGBVECTOR *)eCurFogCol;
+}
+
+//-----------------------------------------------------------------------------
+
+DWORD MWBridge::getScenegraphFogCol()
+{
+    DWORD addr = read_dword(eEnviro) + 0x9c;
+    addr = read_dword(addr) + 0x1c;
+    return read_dword(addr);
 }
 
 //-----------------------------------------------------------------------------

src/mge/mwbridge.h
@@ -44,11 +44,12 @@ public:
     DWORD GetCurrentWeather();
     DWORD GetNextWeather();
     float GetWeatherRatio();
-    RGBVECTOR* CurSkyColVector();
-    RGBVECTOR* CurFogColVector();
+    const RGBVECTOR * getCurrentWeatherSkyCol();
+    const RGBVECTOR * getCurrentWeatherFogCol();
+    DWORD getScenegraphFogCol();
     void setScenegraphFogCol(DWORD c);
     bool CellHasWeather();
-    float* GetWindVector();
+    float * GetWindVector();
     DWORD GetWthrStruct(int wthr);
     int GetWthrString(int wthr, int offset, char str[]);
     void SetWthrString(int wthr, int offset, char str[]);