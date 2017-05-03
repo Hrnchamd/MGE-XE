b543fb5cb56eeab10de6235cfbd9b4eaa182facd
Hrnchamd <a@b.com>
2017-05-03 18:41:56 +0100

    MGEgui macro editor
    Allow backspace in numerals-only trigger delay control.

--- MGEgui/MacroEditor.cs
+++ MGEgui/MacroEditor.cs
@@ -2094,6 +2094,7 @@ namespace MGEgui {
 
         private void NumKeysOnly(object sender,KeyPressEventArgs e) {
             switch(e.KeyChar) {
+                case '\b':
                 case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':
                     return;
                 default:
