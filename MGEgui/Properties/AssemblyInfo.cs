a58468fac8bcdf6e00693e59e9a7fb7a516af7d4
Hrnchamd <a@b.com>
2019-05-26 13:58:06 +0100

    MGEXEgui: Re-target to .NET 4.5.2.

--- MGEgui/Properties/AssemblyInfo.cs
+++ MGEgui/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Graphical interface for changing MGE settings")]
 [assembly: AssemblyCompany("MGE Developers")]
 [assembly: AssemblyProduct("MGEXEgui")]
-[assembly: AssemblyCopyright("Copyright ©  2006 - 2014")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2019")]
 
 [assembly: AssemblyVersion(MGEgui.Statics.versionNumber)]
 [assembly: AssemblyFileVersion(MGEgui.Statics.versionNumber)]
