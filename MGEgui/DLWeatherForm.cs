Hrnchamd/MGE-XE

commit deb0d813d4f38b83f3405c5cff51b7d2aa098a25

MGEXEgui: Actively remove UTF-8 BOM from INI files. Switch to Windows-1252 encoding for morrowind.ini. Prompted by system default encoding being UTF-8-BOM in new .NET versions.

MGEgui/INIFile/INIFile.cs
@@ -53,6 +53,7 @@ namespace MGEgui.INI {
         public static char INIEscape = '\\';
         public static char[] unescapedChars = { '\u0008', '\u0009', '\u000a', '\u000d' };
         public static char[] escapedChars = { 'b', 't', 'n', 'r' };
+        public static Encoding LegacyEncoding = Encoding.GetEncoding(1252);
 
 
         public static INIFile.INIVariableDef iniDefEmpty = new INIFile.INIVariableDef();
@@ -584,12 +585,23 @@ namespace MGEgui.INI {
 
         private void loadINI() {
             iniContent.Clear();
-            StreamReader sr = null;
-            try {
-                sr = new StreamReader(File.OpenRead(fileName), encoding);
-            } catch {
+
+            // Open file and check for unwanted UTF-8 BOM
+            var f = File.OpenRead(fileName);
+            byte[] preamble = new byte[3];
+            f.Read(preamble, 0, 3);
+
+            // Mark for resaving without BOM if present, seek back to start if BOM not present            
+            if (preamble[0] == 0xEF && preamble[1] == 0xBB && preamble[2] == 0xBF) {
+                modified = true;
+            } else {
+                f.Seek(0, SeekOrigin.Begin);
             }
+            
+            // Switch to text stream
+            var sr = new StreamReader(f, encoding);
             INILine.setSection = "";
+
             if (sr != null) {
                 while (!sr.EndOfStream) {
                     INILine line = new INILine(sr.ReadLine());

MGEgui/MainForm.cs
@@ -193,7 +193,7 @@ namespace MGEgui {
         };
 
         private void LoadMWINI() {
-            INIFile mwini = new INIFile(Statics.fn_mwini, mwSettings, System.Text.Encoding.Default);
+            INIFile mwini = new INIFile(Statics.fn_mwini, mwSettings, INIFile.LegacyEncoding);
 
             // Clamp to FPS control maximum
             udFPSLimit.Value = Math.Min(new Decimal(mwini.getKeyValue("FPSLimit")), udFPSLimit.Maximum);
@@ -775,7 +775,7 @@ namespace MGEgui {
         }
 
         private void SaveMWINI() {
-            INIFile mwini = new INIFile(Statics.fn_mwini, mwSettings, System.Text.Encoding.Default, true);
+            INIFile mwini = new INIFile(Statics.fn_mwini, mwSettings, INIFile.LegacyEncoding, true);
 
             mwini.setKey("FPSLimit", (int)udFPSLimit.Value);
             mwini.setKey("SSEnable", cbScreenshots.Checked);

commit 975f45433f5d220cedd34111444cb06c1a758c63

MGEXEgui: Fix unstartable program when MGE.ini is not present, and crash when accessing Weather Settings. Bug introduced in deb0d813d4f38b83f3405c5cff51b7d2aa098a25.

MGEgui/DLWeatherForm.cs
@@ -101,6 +101,8 @@ namespace MGEgui {
 
         private void LoadSettings() {
             INIFile iniFile = new INIFile(Statics.fn_inifile, iniWeatherSettings);
+
+            // Load settings into controls
             foreach (Control ctl in this.Controls["gbWind"].Controls) {
                 if (ctl.Name.Substring(0, 2) != "ud") {
                     continue;
@@ -122,7 +124,9 @@ namespace MGEgui {
                 NumericUpDown ud = (NumericUpDown)ctl;
                 ud.Value = (decimal)iniFile.getKeyValue(ud.Name.Substring(2));
             }
-            iniFile = new INIFile("NUL", iniWeatherSettings);
+            
+            // Load settings for default buttons
+            iniFile = new INIFile(Statics.fn_nul, iniWeatherSettings);
             foreach (Control ctl in this.Controls["gbWind"].Controls) {
                 if (ctl.Name.Substring(0, 2) != "ud") {
                     continue;
@@ -200,6 +204,7 @@ namespace MGEgui {
         private void bSave_Click(object sender, EventArgs e) {
             INIFile iniFile = new INIFile(Statics.fn_inifile, iniWeatherSettings, true);
             iniFile.initialize();
+            
             foreach (Control ctl in this.Controls["gbWind"].Controls) {
                 if (ctl.Name.Substring(0, 2) != "ud") {
                     continue;
@@ -221,6 +226,7 @@ namespace MGEgui {
                 NumericUpDown ud = (NumericUpDown)ctl;
                 iniFile.setKey(ud.Name.Substring(2), (double)ud.Value);
             }
+            
             iniFile.save();
             Close();
         }

MGEgui/INIFile/INIFile.cs
@@ -586,36 +586,44 @@ namespace MGEgui.INI {
         private void loadINI() {
             iniContent.Clear();
 
-            // Open file and check for unwanted UTF-8 BOM
-            var f = File.OpenRead(fileName);
-            byte[] preamble = new byte[3];
-            f.Read(preamble, 0, 3);
-
-            // Mark for resaving without BOM if present, seek back to start if BOM not present            
-            if (preamble[0] == 0xEF && preamble[1] == 0xBB && preamble[2] == 0xBF) {
-                modified = true;
-            } else {
-                f.Seek(0, SeekOrigin.Begin);
+            // Explicitly test for NUL instead of catching exceptions
+            if (string.Equals(fileName, "NUL", StringComparison.OrdinalIgnoreCase)) {
+                return;
             }
-            
-            // Switch to text stream
-            var sr = new StreamReader(f, encoding);
-            INILine.setSection = "";
 
-            if (sr != null) {
-                while (!sr.EndOfStream) {
-                    INILine line = new INILine(sr.ReadLine());
-                    INIVariableDef vd = getVarDefn(line.section, line.key);
-                    line.defVal = vd.defValue;
-                    line.varType = vd.type;
-                    line.varDict = vd.dict;
-                    line.varBool = vd.boolType;
-                    line.useMinMax = vd.useMinMax;
-                    line.max = vd.max;
-                    line.min = vd.min;
-                    iniContent.Add(line);
+            // Open file and check for unwanted UTF-8 BOM
+            try {
+                using (var f = File.OpenRead(fileName)) {
+                    byte[] preamble = new byte[3];
+                    f.Read(preamble, 0, 3);
+        
+                    // Mark for resaving without BOM if present, seek back to start if BOM not present            
+                    if (preamble[0] == 0xEF && preamble[1] == 0xBB && preamble[2] == 0xBF) {
+                        modified = true;
+                    } else {
+                        f.Seek(0, SeekOrigin.Begin);
+                    }
+                    
+                    // Switch to text stream
+                    using (var sr = new StreamReader(f, encoding)) {
+                        INILine.setSection = "";
+        
+                        while (!sr.EndOfStream) {
+                            INILine line = new INILine(sr.ReadLine());
+                            INIVariableDef vd = getVarDefn(line.section, line.key);
+                            line.defVal = vd.defValue;
+                            line.varType = vd.type;
+                            line.varDict = vd.dict;
+                            line.varBool = vd.boolType;
+                            line.useMinMax = vd.useMinMax;
+                            line.max = vd.max;
+                            line.min = vd.min;
+                            iniContent.Add(line);
+                        }
+                    }
                 }
-                sr.Close();
+            } catch (FileNotFoundException) {
+                // Assume generating new INI
             }
         }
 
@@ -657,18 +665,13 @@ namespace MGEgui.INI {
 
         public bool save() {
             if (modified) {
-                StreamWriter sw = null;
                 try {
-                    sw = new StreamWriter(File.Create(fileName), encoding);
-                } catch {
-                    return false;
-                }
-                if (sw != null) {
-                    foreach (INILine line in iniContent) {
-                        sw.WriteLine(line.entry + line.comment);
+                    using (var sw = new StreamWriter(File.Create(fileName), encoding)) {
+                        foreach (INILine line in iniContent) {
+                            sw.WriteLine(line.entry + line.comment);
+                        }
                     }
-                    sw.Close();
-                } else {
+                } catch {
                     return false;
                 }
             }