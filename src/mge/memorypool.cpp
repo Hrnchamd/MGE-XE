9c4d08bd9cf5151551e5934dfd2da91866e6e58c
Hrnchamd <a@b.com>
2019-06-05 14:47:27 +0100

    Tidy up memorypool.

--- src/mge/memorypool.cpp
+++ src/mge/memorypool.cpp
@@ -4,14 +4,14 @@
 
 //-----------------------------------------------------------------------------
 
-MemoryPool::MemoryBlock::MemoryBlock( MemoryPool* owner_pool ) :
+MemoryPool::MemoryBlock::MemoryBlock(MemoryPool* owner_pool) :
     next_alloc(0),
-    next_block(0),
+    next_block(nullptr),
     owner(owner_pool) {
 
     // Allocate the memory block
-    data = new char[ owner->blk_size ];
-    assert( data != 0 );
+    data = new char[owner->blk_size];
+    assert(data != nullptr);
 }
 
 //-----------------------------------------------------------------------------
@@ -19,13 +19,15 @@ MemoryPool::MemoryBlock::MemoryBlock( MemoryPool* owner_pool ) :
 MemoryPool::MemoryBlock::~MemoryBlock() {
     // Free the memory block
     delete [] data;
+    data = nullptr;
+    owner = nullptr;
 }
 
 //-----------------------------------------------------------------------------
 
 void* MemoryPool::MemoryBlock::Alloc() {
     // Check if we have any more free objects to return
-    if ( next_alloc < owner->objs_per_block ) {
+    if (next_alloc < owner->objs_per_block) {
         // Increment next_alloc and return the available object
         void* ret = data + (next_alloc * owner->obj_size);
         ++next_alloc;
@@ -38,20 +40,19 @@ void* MemoryPool::MemoryBlock::Alloc() {
 
 //-----------------------------------------------------------------------------
 
-MemoryPool::MemoryPool( size_t object_size, size_t objects_per_block  ) {
-
-    obj_size = object_size;
-    objs_per_block = objects_per_block;
+MemoryPool::MemoryPool(size_t object_size, size_t objects_per_block) :
+    obj_size(object_size),
+    objs_per_block(objects_per_block) {
 
     // Align size to 32-byte boundaries
-    if ( obj_size % 32 != 0 ) {
+    if (obj_size % 32 != 0) {
         obj_size += 32 - (obj_size % 32);
     }
 
     // Make sure we use a block size that's padded to 4096 to match the windows memory page size.
     blk_size = objs_per_block * obj_size;
 
-    if ( blk_size % 4096 != 0 ) {
+    if (blk_size % 4096 != 0) {
         blk_size += 4096 - (blk_size % 4096);
     }
 
@@ -74,14 +75,14 @@ void MemoryPool::AllocFirstBlock() {
 void* MemoryPool::Alloc() {
     // First attempt to allocate from the last block (the one that still may have free spaces)
     void* new_obj = last_block->Alloc();
-    if ( new_obj ) {
+    if (new_obj) {
         // Allocation was successful
         return new_obj;
     }
 
     // Allocation was not successful, so we need a new block
     MemoryBlock* new_block = new MemoryBlock(this);
-    assert(new_block != 0);
+    assert(new_block != nullptr);
 
     // Hook up the new block to the linked list
     last_block->next_block = new_block;
@@ -106,12 +107,16 @@ void MemoryPool::Flush() {
 void MemoryPool::FreeAllBlocks() {
     // Traverse the linked list of blocks, freeing each one
     MemoryBlock* current_block = first_block;
-    MemoryBlock* next_block = 0;
-    while (current_block != 0) {
+    MemoryBlock* next_block = nullptr;
+
+    while (current_block != nullptr) {
         next_block = current_block->next_block;
         delete current_block;
         current_block = next_block;
     }
+
+    first_block = nullptr;
+    last_block = nullptr;
 }
 
 //-----------------------------------------------------------------------------
