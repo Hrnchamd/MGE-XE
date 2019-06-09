Hrnchamd/MGE-XE

commit e59e74b744c82c6f2a638bcbab7b9cf355e29e2c

Change Morrowind re-rendering to use worldview matrices and view space lighting. Avoids model cracks caused by numerical accuracy issues.

src/mge/ffeshader.cpp
@@ -15,7 +15,8 @@ unordered_map<FixedFunctionShader::ShaderKey, ID3DXEffect *, FixedFunctionShader
 FixedFunctionShader::ShaderLRU FixedFunctionShader::shaderLRU;
 ID3DXEffect *FixedFunctionShader::effectDefaultPurple;
 
-D3DXHANDLE FixedFunctionShader::ehWorld, FixedFunctionShader::ehVertexBlendState, FixedFunctionShader::ehVertexBlendPalette;
+D3DXHANDLE FixedFunctionShader::ehWorld, FixedFunctionShader::ehWorldView;
+D3DXHANDLE FixedFunctionShader::ehVertexBlendState, FixedFunctionShader::ehVertexBlendPalette;
 D3DXHANDLE FixedFunctionShader::ehTex0, FixedFunctionShader::ehTex1, FixedFunctionShader::ehTex2, FixedFunctionShader::ehTex3, FixedFunctionShader::ehTex4, FixedFunctionShader::ehTex5;
 D3DXHANDLE FixedFunctionShader::ehMaterialDiffuse, FixedFunctionShader::ehMaterialAmbient, FixedFunctionShader::ehMaterialEmissive;
 D3DXHANDLE FixedFunctionShader::ehLightSceneAmbient, FixedFunctionShader::ehLightSunDiffuse, FixedFunctionShader::ehLightDiffuse;
@@ -61,6 +62,7 @@ bool FixedFunctionShader::init(IDirect3DDevice *d, ID3DXEffectPool *pool)
     ehTex4 = effect->GetParameterByName(0, "tex4");
     ehTex5 = effect->GetParameterByName(0, "tex5");
 
+    ehWorldView = effect->GetParameterByName(0, "worldview");
     ehMaterialDiffuse = effect->GetParameterByName(0, "materialDiffuse");
     ehMaterialAmbient = effect->GetParameterByName(0, "materialAmbient");
     ehMaterialEmissive = effect->GetParameterByName(0, "materialEmissive");
@@ -148,10 +150,12 @@ void FixedFunctionShader::renderMorrowind(const RenderedState *rs, const Fragmen
         {
             memcpy(&bufferDiffuse[pointLightCount], &light->diffuse, sizeof(light->diffuse));
 
-            // Scatter position vectors for vectorization
-            bufferPosition[pointLightCount] = light->position.x;
-            bufferPosition[pointLightCount + MaxLights] = light->position.y;
-            bufferPosition[pointLightCount + 2*MaxLights] = light->position.z;
+            // Transform to view space and scatter position vectors for vectorization
+            D3DXVECTOR3 lightViewPos;
+            D3DXVec3TransformCoord(&lightViewPos, (D3DXVECTOR3*)&light->position, &rs->viewTransform);
+            bufferPosition[pointLightCount] = lightViewPos.x;
+            bufferPosition[pointLightCount + MaxLights] = lightViewPos.y;
+            bufferPosition[pointLightCount + 2*MaxLights] = lightViewPos.z;
 
             // Scatter attenuation factors for vectorization
             if(light->falloff.x > 0)
@@ -201,7 +205,10 @@ void FixedFunctionShader::renderMorrowind(const RenderedState *rs, const Fragmen
         }
         else if(light->type == D3DLIGHT_DIRECTIONAL)
         {
-            effectFFE->SetFloatArray(ehLightSunDirection, (const float *)&light->position, 3);
+            D3DXVECTOR3 sunViewDirection;
+            D3DXVec3TransformNormal(&sunViewDirection, (D3DXVECTOR3*)&light->position, &rs->viewTransform);
+            effectFFE->SetFloatArray(ehLightSunDirection, sunViewDirection, 3);
+
             sunDiffuse = light->diffuse;
             ambient.r += light->ambient.x;
             ambient.g += light->ambient.y;
@@ -263,9 +270,19 @@ void FixedFunctionShader::renderMorrowind(const RenderedState *rs, const Fragmen
     // Set common state and render
     effectFFE->SetInt(ehVertexBlendState, rs->vertexBlendState);
     if(rs->vertexBlendState)
-        effectFFE->SetMatrixArray(ehVertexBlendPalette, rs->worldTransforms, 4);
+    {
+        D3DXMATRIX worldView[4];
+        for(n = 0; n != 4; ++n)
+            worldView[n] = rs->worldTransforms[n] * rs->viewTransform;
+
+        effectFFE->SetMatrixArray(ehVertexBlendPalette, worldView, 4);
+    }
     else
+    {
+        D3DXMATRIX worldView = rs->worldTransforms[0] * rs->viewTransform;
         effectFFE->SetMatrix(ehWorld, &rs->worldTransforms[0]);
+        effectFFE->SetMatrix(ehWorldView, &worldView);
+    }
 
     UINT passes;
     effectFFE->Begin(&passes, D3DXFX_DONOTSAVESTATE);
@@ -351,9 +368,9 @@ ID3DXEffect * FixedFunctionShader::generateMWShader(const ShaderKey& sk)
     buf.str(string());
 
     if(sk.usesSkinning)
-        buf << "worldpos = skinnedVertex(IN.pos, IN.blendweights); normal = skinnedNormal(IN.nrm, IN.blendweights);";
+        buf << "viewpos = skinnedVertex(IN.pos, IN.blendweights); normal = skinnedNormal(IN.nrm, IN.blendweights);";
     else
-        buf << "worldpos = rigidVertex(IN.pos); normal = rigidNormal(IN.nrm);";
+        buf << "viewpos = rigidVertex(IN.pos); normal = rigidNormal(IN.nrm);";
 
     genTransform = buf.str();
 
@@ -377,10 +394,10 @@ ID3DXEffect * FixedFunctionShader::generateMWShader(const ShaderKey& sk)
             buf << "texgenNormal(normal); ";
             break;
         case D3DTSS_TCI_CAMERASPACEPOSITION >> 16:
-            buf << "texgenPosition(worldpos); ";
+            buf << "texgenPosition(viewpos); ";
             break;
         case D3DTSS_TCI_CAMERASPACEREFLECTIONVECTOR >> 16:
-            buf << "texgenReflection(worldpos, normal); ";
+            buf << "texgenReflection(viewpos, normal); ";
             break;
         case D3DTSS_TCI_SPHEREMAP >> 16:
             buf << "texgenSphere(" << texRouting[texGenSrcIndex] << "); ";

src/mge/ffeshader.h
@@ -14,6 +14,7 @@ struct RenderedState
     DWORD zWrite, cullMode;
     DWORD vertexBlendState;
     D3DXMATRIX worldTransforms[4];
+    D3DXMATRIX viewTransform;
     BYTE blendEnable, srcBlend, destBlend;
     BYTE alphaTest, alphaFunc, alphaRef;
     BYTE useLighting, useFog, matSrcDiffuse, matSrcEmissive;
@@ -110,7 +111,8 @@ class FixedFunctionShader
     static ShaderLRU shaderLRU;
     static ID3DXEffect *effectDefaultPurple;
 
-    static D3DXHANDLE ehWorld, ehVertexBlendState, ehVertexBlendPalette;
+    static D3DXHANDLE ehWorld, ehWorldView;
+    static D3DXHANDLE ehVertexBlendState, ehVertexBlendPalette;
     static D3DXHANDLE ehTex0, ehTex1, ehTex2, ehTex3, ehTex4, ehTex5;
     static D3DXHANDLE ehMaterialDiffuse, ehMaterialAmbient, ehMaterialEmissive;
     static D3DXHANDLE ehLightSceneAmbient, ehLightSunDiffuse, ehLightSunDirection;

src/mge/mged3d8device.cpp
@@ -621,6 +621,7 @@ void captureTransform(D3DTRANSFORMSTATETYPE a, const D3DMATRIX *b)
         case D3DTS_WORLDMATRIX(1): rs.worldTransforms[1] = *b; break;
         case D3DTS_WORLDMATRIX(2): rs.worldTransforms[2] = *b; break;
         case D3DTS_WORLDMATRIX(3): rs.worldTransforms[3] = *b; break;
+        case D3DTS_VIEW: rs.viewTransform = *b; break;
     }
 }
 

src/mge/renderdepth.cpp
@@ -96,6 +96,8 @@ void DistantLand::renderDepthRecorded()
     for(std::vector<RenderedState>::const_iterator i = recordMW.begin(); i != recordMW.end(); ++i)
     {
         // Set variables in main effect; variables are shared via effect pool
+
+        // Only bind texture for alphas
         if((i->alphaTest || i->blendEnable) && i->texture)
         {
             effect->SetTexture(ehTex0, i->texture);
@@ -109,9 +111,15 @@ void DistantLand::renderDepthRecorded()
             effect->SetFloat(ehAlphaRef, -1.0f);
         }
 
+        // Skin using worldview matrices for numerical accuracy
+        D3DXMATRIX worldView[4];
+        const int count = (i->vertexBlendState <= 3) ? i->vertexBlendState + 1 : 1;
+        for(int n = 0; n != count; ++n)
+            worldView[n] = i->worldTransforms[n] * mwView;
+
         effect->SetBool(ehHasBones, i->vertexBlendState != 0);
         effect->SetInt(ehVertexBlendState, i->vertexBlendState);
-        effect->SetMatrixArray(ehVertexBlendPalette, i->worldTransforms, 4);
+        effect->SetMatrixArray(ehVertexBlendPalette, worldView, 4);
         effectDepth->CommitChanges();
 
         device->SetRenderState(D3DRS_CULLMODE, i->cullMode);

src/mge/rendershadow.cpp
@@ -162,14 +162,23 @@ void DistantLand::renderShadowLayer(int layer, float radius, const D3DXMATRIX *i
 // renderShadow - Renders shadows (using blending) over Morrowind shadow receivers
 void DistantLand::renderShadow()
 {
-    effect->SetMatrixArray(ehShadowViewproj, smViewproj, 2);
+    // Supply view space -> shadow clip space matrix
+    D3DXMATRIX inverseView, viewToShadow[2];
+    D3DXMatrixInverse(&inverseView, NULL, &mwView);
+    viewToShadow[0] = inverseView * smViewproj[0];
+    viewToShadow[1] = inverseView * smViewproj[1];
+    effect->SetMatrixArray(ehShadowViewproj, viewToShadow, 2);
+
+    // Bind filtered ESM
     effect->SetTexture(ehTex3, texSoftShadow);
 
     for(std::vector<RenderedState>::const_iterator i = recordMW.begin(); i != recordMW.end(); ++i)
     {
+        // Additive alphas do not receive shadows
         if(i->blendEnable && i->destBlend == D3DBLEND_ONE)
             continue;
 
+        // Only bind texture for alphas
         if((i->alphaTest || i->blendEnable) && i->texture)
         {
             effect->SetTexture(ehTex0, i->texture);
@@ -183,9 +192,15 @@ void DistantLand::renderShadow()
             effect->SetFloat(ehAlphaRef, -1.0f);
         }
 
+        // Skin using worldview matrices for numerical accuracy
+        D3DXMATRIX worldView[4];
+        const int count = (i->vertexBlendState <= 3) ? i->vertexBlendState + 1 : 1;
+        for(int n = 0; n != count; ++n)
+            worldView[n] = i->worldTransforms[n] * mwView;
+
         effect->SetBool(ehHasBones, i->vertexBlendState != 0);
         effect->SetInt(ehVertexBlendState, i->vertexBlendState);
-        effect->SetMatrixArray(ehVertexBlendPalette, i->worldTransforms, 4);
+        effect->SetMatrixArray(ehVertexBlendPalette, worldView, 4);
         effect->CommitChanges();
 
         // Ignore two-sided poly (cull none) mode, shadow casters are drawn with CW culling only,

assets/shaders/XE Common.fx
@@ -1,6 +1,6 @@
 
 // XE Common.fx
-// MGE XE 0.9
+// MGE XE 0.11
 // Shared structures and functions
 
 
@@ -10,7 +10,7 @@
 
 shared float2 rcpres;
 shared float shadowRcpRes;
-shared matrix view, proj, world;
+shared matrix world, view, proj;
 shared matrix vertexblendpalette[4];
 shared matrix shadowviewproj[2];
 shared bool hasalpha, hasbones;
@@ -18,7 +18,7 @@ shared float alpharef;
 shared int vertexblendstate;
 
 shared float3 EyePos, FootPos;
-shared float3 SunVec, SunCol, SunAmb;
+shared float3 SunVec, SunVecView, SunCol, SunAmb;
 shared float3 SkyCol, FogCol1, FogCol2;
 shared float FogStart, FogRange;
 shared float nearFogStart, nearFogRange;
@@ -264,6 +264,7 @@ float4 instancedMul(float4 pos, float4 m0, float4 m1, float4 m2)
 
 //------------------------------------------------------------
 // Skinning, fixed-function
+// Uses worldview matrices for numerical accuracy
 
 float4 skin(float4 pos, float4 blend)
 {
@@ -274,16 +275,16 @@ float4 skin(float4 pos, float4 blend)
     else if(vertexblendstate == 3)
         blend[3] = 1 - (blend[0] + blend[1] + blend[2]);
 
-    float4 worldpos = mul(pos, vertexblendpalette[0]) * blend[0];
+    float4 viewpos = mul(pos, vertexblendpalette[0]) * blend[0];
 
     if(vertexblendstate >= 1)
-        worldpos += mul(pos, vertexblendpalette[1]) * blend[1];
+        viewpos += mul(pos, vertexblendpalette[1]) * blend[1];
     if(vertexblendstate >= 2)
-        worldpos += mul(pos, vertexblendpalette[2]) * blend[2];
+        viewpos += mul(pos, vertexblendpalette[2]) * blend[2];
     if(vertexblendstate >= 3)
-        worldpos += mul(pos, vertexblendpalette[3]) * blend[3];
+        viewpos += mul(pos, vertexblendpalette[3]) * blend[3];
         
-    return worldpos;
+    return viewpos;
 }
 
 //------------------------------------------------------------

assets/shaders/XE Depth.fx
@@ -1,6 +1,6 @@
 
 // XE Depth.fx
-// MGE XE 0.9
+// MGE XE 0.11
 // Depth buffer render sequence
 
 #include "XE Common.fx"
@@ -33,17 +33,16 @@ struct DepthVertOut
 DepthVertOut DepthMWVS (in MorrowindVertIn IN)
 {
     DepthVertOut OUT;
-    float4 worldpos;
+    float4 viewpos;
 
     // Skin mesh if required
     if(hasbones)
-        worldpos = skin(IN.pos, IN.blendweights);
+        viewpos = skin(IN.pos, IN.blendweights);
     else
-        worldpos = mul(IN.pos, vertexblendpalette[0]);
+        viewpos = mul(IN.pos, vertexblendpalette[0]);
     
-    // Transform to world space
-    OUT.pos = mul(worldpos, view);
-    OUT.pos = mul(OUT.pos, proj);
+    // Transform and output depth
+    OUT.pos = mul(viewpos, proj);
     OUT.depth = OUT.pos.w;
     OUT.texcoords = IN.texcoords;
 

assets/shaders/XE FixedFuncEmu.fx
@@ -1,11 +1,12 @@
 
 // XE FixedFuncEmu.fx
-// MGE XE 0.9
+// MGE XE 0.11
 // Replacement shaders for Morrowind's object rendering
 
 #include "XE Common.fx"
 
 shared texture tex4, tex5;
+shared matrix worldview;
 shared float4 materialDiffuse, materialAmbient, materialEmissive;
 shared float3 lightSceneAmbient, lightSunDiffuse, lightDiffuse[8];
 shared float4 lightAmbient[2];
@@ -29,8 +30,8 @@ sampler sampFFE5 = sampler_state { texture = <tex5>; };
 #ifdef VERIFY
 #define FFE_VB_COUPLING float2 texcoord0 : TEXCOORD0; float4 col : COLOR;
 #define FFE_SHADER_COUPLING float4 texcoord01 : TEXCOORD0; float4 col : COLOR;
-#define FFE_TRANSFORM_SKIN worldpos = rigidVertex(IN.pos); normal = rigidNormal(IN.nrm);
-#define FFE_TEXCOORDS_TEXGEN float3 texgen = texgenReflection(worldpos, normal); texgen = mul(float4(texgen, 1), texgenTransform).xyz; OUT.texcoord01 = float4(IN.texcoord0, texgen.xy);
+#define FFE_TRANSFORM_SKIN viewpos = rigidVertex(IN.pos); normal = rigidNormal(IN.nrm);
+#define FFE_TEXCOORDS_TEXGEN float3 texgen = texgenReflection(viewpos, normal); texgen = mul(float4(texgen, 1), texgenTransform).xyz; OUT.texcoord01 = float4(IN.texcoord0, texgen.xy);
 #define FFE_VERTEX_COLOUR OUT.col = IN.col;
 #define FFE_LIGHTS_ACTIVE 8
 #define FFE_VERTEX_MATERIAL diffuse = vertexMaterialDiffAmb(d, a, IN.col);
@@ -41,7 +42,7 @@ sampler sampFFE5 = sampler_state { texture = <tex5>; };
 #ifdef FFE_ERROR_MATERIAL
 #define FFE_VB_COUPLING
 #define FFE_SHADER_COUPLING
-#define FFE_TRANSFORM_SKIN worldpos = rigidVertex(IN.pos); normal = float3(0, 0, 1);
+#define FFE_TRANSFORM_SKIN viewpos = rigidVertex(IN.pos); normal = float3(0, 0, 1);
 #define FFE_TEXCOORDS_TEXGEN
 #define FFE_VERTEX_COLOUR
 #define FFE_LIGHTS_ACTIVE 0
@@ -54,17 +55,17 @@ sampler sampFFE5 = sampler_state { texture = <tex5>; };
 //------------------------------------------------------------
 // Transform library functions
 
-// Vertex transforms, note that scaled normals are normalized in the pixel shader
-float4 rigidVertex(float4 pos) { return mul(pos, world); }
-float3 rigidNormal(float3 normal) { return mul(float4(normal, 0), world).xyz; }
+// Vertex transform to view space, note that scaled normals are normalized in the pixel shader
+float4 rigidVertex(float4 pos) { return mul(pos, worldview); }
+float3 rigidNormal(float3 normal) { return mul(float4(normal, 0), worldview).xyz; }
 
 float4 skinnedVertex(float4 pos, float4 weights) { return skin(pos, weights); }
 float3 skinnedNormal(float3 normal, float4 weights) { return skin(float4(normal, 0), weights).xyz; }
 
-// Texgens with world space inputs, normals must be normalized due to ubiquitous scaling matrices
-float3 texgenNormal(float3 normal) { return mul(float4(normalize(normal), 0), view).xyz; }
-float3 texgenPosition(float4 pos) { return mul(pos, view).xyz; }
-float3 texgenReflection(float4 pos, float3 normal) { float3 r = reflect(normalize(pos.xyz - EyePos), normalize(normal)); return mul(float4(r, 0), view).xyz; }
+// Texgens with view space inputs, normals must be normalized due to non-uniform scaling matrices
+float3 texgenNormal(float3 normal) { return normalize(normal); }
+float3 texgenPosition(float4 pos) { return pos.xyz; }
+float3 texgenReflection(float4 pos, float3 normal) { return reflect(normalize(pos.xyz), normalize(normal)); }
 float3 texgenSphere(float2 tex) { return float3(0.5 * tex + 0.5, 0); }
 
 //------------------------------------------------------------
@@ -164,13 +165,12 @@ FFEPixel PerPixelVS(FFEVertIn IN)
     FFEPixel OUT;
 
     // Transforms
-    float4 worldpos;
+    float4 viewpos;
     float3 normal;
     /* template */ FFE_TRANSFORM_SKIN
     
-    OUT.pos = mul(worldpos, view);
-    float dist = length(OUT.pos);
-    OUT.pos = mul(OUT.pos, proj);
+    float dist = length(viewpos);
+    OUT.pos = mul(viewpos, proj);
     OUT.nrm_fog = float4(normal, fogMWScalar(dist));
     
     // Texcoord routing and texgen
@@ -182,9 +182,9 @@ FFEPixel PerPixelVS(FFEVertIn IN)
     // Point lighting setup, vectorized
     for(int i = 0; i != LGs; ++i)
     {
-        OUT.lightvec[3*i + 0] = lightPosition[i + 0] - worldpos.x;
-        OUT.lightvec[3*i + 1] = lightPosition[i + 2] - worldpos.y;
-        OUT.lightvec[3*i + 2] = lightPosition[i + 4] - worldpos.z;
+        OUT.lightvec[3*i + 0] = lightPosition[i + 0] - viewpos.x;
+        OUT.lightvec[3*i + 1] = lightPosition[i + 2] - viewpos.y;
+        OUT.lightvec[3*i + 2] = lightPosition[i + 4] - viewpos.z;
     }
     
     return OUT;

commit 0b6c537eae9b79c9515b174de30513be4e84686d

Fix performance regression caused by e59e74b744c82c6f2a638bcbab7b9cf355e29e2c. Excessive matrix multiplications per draw call was increasing frame time by 15% worst case in towns.

src/mge/ffeshader.cpp
@@ -259,16 +259,10 @@ void FixedFunctionShader::renderMorrowind(const RenderedState* rs, const Fragmen
     // Set common state and render
     effectFFE->SetInt(ehVertexBlendState, rs->vertexBlendState);
     if (rs->vertexBlendState) {
-        D3DXMATRIX worldView[4];
-        for (n = 0; n != 4; ++n) {
-            worldView[n] = rs->worldTransforms[n] * rs->viewTransform;
-        }
-
-        effectFFE->SetMatrixArray(ehVertexBlendPalette, worldView, 4);
+        effectFFE->SetMatrixArray(ehVertexBlendPalette, rs->worldViewTransforms, 4);
     } else {
-        D3DXMATRIX worldView = rs->worldTransforms[0] * rs->viewTransform;
         effectFFE->SetMatrix(ehWorld, &rs->worldTransforms[0]);
-        effectFFE->SetMatrix(ehWorldView, &worldView);
+        effectFFE->SetMatrix(ehWorldView, &rs->worldViewTransforms[0]);
     }
 
     UINT passes;

src/mge/ffeshader.h
@@ -18,6 +18,7 @@ struct RenderedState {
     DWORD vertexBlendState;
     D3DXMATRIX worldTransforms[4];
     D3DXMATRIX viewTransform;
+    D3DXMATRIX worldViewTransforms[4];
     BYTE blendEnable, srcBlend, destBlend;
     BYTE alphaTest, alphaFunc, alphaRef;
     BYTE useLighting, useFog, matSrcDiffuse, matSrcEmissive;

src/mge/mged3d8device.cpp
@@ -632,15 +632,19 @@ void captureTransform(D3DTRANSFORMSTATETYPE a, const D3DMATRIX* b) {
     switch (a) {
     case D3DTS_WORLDMATRIX(0):
         rs.worldTransforms[0] = *b;
+        D3DXMatrixMultiply(&rs.worldViewTransforms[0], static_cast<const D3DXMATRIX*>(b), &rs.viewTransform);
         break;
     case D3DTS_WORLDMATRIX(1):
         rs.worldTransforms[1] = *b;
+        D3DXMatrixMultiply(&rs.worldViewTransforms[1], static_cast<const D3DXMATRIX*>(b), &rs.viewTransform);
         break;
     case D3DTS_WORLDMATRIX(2):
         rs.worldTransforms[2] = *b;
+        D3DXMatrixMultiply(&rs.worldViewTransforms[2], static_cast<const D3DXMATRIX*>(b), &rs.viewTransform);
         break;
     case D3DTS_WORLDMATRIX(3):
         rs.worldTransforms[3] = *b;
+        D3DXMatrixMultiply(&rs.worldViewTransforms[3], static_cast<const D3DXMATRIX*>(b), &rs.viewTransform);
         break;
     case D3DTS_VIEW:
         rs.viewTransform = *b;

src/mge/renderdepth.cpp
@@ -102,15 +102,9 @@ void DistantLand::renderDepthRecorded() {
         }
 
         // Skin using worldview matrices for numerical accuracy
-        D3DXMATRIX worldView[4];
-        const int count = (i.vertexBlendState <= 3) ? i.vertexBlendState + 1 : 1;
-        for (int n = 0; n != count; ++n) {
-            worldView[n] = i.worldTransforms[n] * mwView;
-        }
-
         effect->SetBool(ehHasBones, i.vertexBlendState != 0);
         effect->SetInt(ehVertexBlendState, i.vertexBlendState);
-        effect->SetMatrixArray(ehVertexBlendPalette, worldView, 4);
+        effect->SetMatrixArray(ehVertexBlendPalette, i.worldViewTransforms, 4);
         effectDepth->CommitChanges();
 
         device->SetRenderState(D3DRS_CULLMODE, i.cullMode);

src/mge/rendershadow.cpp
@@ -192,15 +192,9 @@ void DistantLand::renderShadow() {
         }
 
         // Skin using worldview matrices for numerical accuracy
-        D3DXMATRIX worldView[4];
-        const int count = (i.vertexBlendState <= 3) ? i.vertexBlendState + 1 : 1;
-        for (int n = 0; n != count; ++n) {
-            worldView[n] = i.worldTransforms[n] * mwView;
-        }
-
         effect->SetBool(ehHasBones, i.vertexBlendState != 0);
         effect->SetInt(ehVertexBlendState, i.vertexBlendState);
-        effect->SetMatrixArray(ehVertexBlendPalette, worldView, 4);
+        effect->SetMatrixArray(ehVertexBlendPalette, i.worldViewTransforms, 4);
         effect->CommitChanges();
 
         // Ignore two-sided poly (cull none) mode, shadow casters are drawn with CW culling only,