982b2d661d9c2f7170b94da1834041628ac8eb91
Hrnchamd <a@b.com>
2021-05-14 22:44:21 +0100

    Version bump.

--- MGEgui/Properties/AssemblyInfo.cs
+++ MGEgui/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("MGEXEgui")]
-[assembly: AssemblyDescription("Graphical interface for changing MGE settings")]
-[assembly: AssemblyCompany("MGE Developers")]
-[assembly: AssemblyProduct("MGEXEgui")]
-[assembly: AssemblyCopyright("Copyright © 2006 - 2019")]
-
-[assembly: AssemblyVersion(MGEgui.Statics.versionNumber)]
-[assembly: AssemblyFileVersion(MGEgui.Statics.versionNumber)]
-
-[assembly: ComVisible(false)]
-[assembly: System.CLSCompliant(false)]
+[assembly: AssemblyTitle ("MGEXEgui")]
+[assembly: AssemblyDescription ("Graphical interface for changing MGE settings")]
+[assembly: AssemblyCompany ("MGE Developers")]
+[assembly: AssemblyProduct ("MGEXEgui")]
+[assembly: AssemblyCopyright ("Copyright Â© 2006 - 2021")]
+[assembly: AssemblyVersion (MGEgui.Statics.versionNumber)]
+[assembly: AssemblyFileVersion (MGEgui.Statics.versionNumber)]
+[assembly: ComVisible (false)]
+[assembly: System.CLSCompliant (false)]
